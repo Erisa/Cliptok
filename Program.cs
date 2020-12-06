5821ffb8970682404e6a79370881f9ed8e73cb75
Erisa A <seriel@erisa.moe>
2020-12-06 19:28:10 +0000

    Enable all intents, should lessen mute related issues

--- Program.cs
+++ Program.cs
@@ -49,7 +49,8 @@ namespace MicrosoftBot
             {
                 Token = cfgjson.Core.Token,
                 TokenType = TokenType.Bot,
-                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug
+                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
+                Intents = DiscordIntents.All
             });
 
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
