8083e790454b1e3faaad1e55d6aad54c026662ea
Erisa A <seriel@erisa.moe>
2021-05-14 00:38:24 +0100

    Add EXPERIMENTAL /ban command

--- Modules/SlashCommands.cs
+++ Modules/SlashCommands.cs
@@ -22,13 +22,18 @@ namespace Cliptok.Modules
              [Option("channel", "The channel to warn the user in, implied if not supplied.")] DiscordChannel channel = null
             )
         {
-            DiscordInteractionResponseBuilder eout;
+            // Initial response to avoid the 3 second timeout, will edit later.
+            var eout = new DiscordInteractionResponseBuilder().AsEphemeral(true);
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, eout);
+
+            // Edits need a webhook rather than interaction..?
+            DiscordWebhookBuilder webhookOut;
 
             if (Warnings.GetPermLevel(ctx.Member) < ServerPermLevel.TrialMod)
             {
-                eout = new DiscordInteractionResponseBuilder().WithContent($"{Program.cfgjson.Emoji.NoPermissions} Invalid permissions to use command **{ctx.CommandName}**!");
-                eout.IsEphemeral = true;
-                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, eout);
+                webhookOut = new DiscordWebhookBuilder()
+                    .WithContent($"{Program.cfgjson.Emoji.NoPermissions} Invalid permissions to use command **{ctx.CommandName}**!");
+                await ctx.EditResponseAsync(webhookOut);
                 return;
             }
 
@@ -39,9 +44,8 @@ namespace Cliptok.Modules
                 targetMember = await ctx.Guild.GetMemberAsync(user.Id);
                 if (Warnings.GetPermLevel(ctx.Member) == ServerPermLevel.TrialMod && (Warnings.GetPermLevel(targetMember) >= ServerPermLevel.TrialMod || targetMember.IsBot))
                 {
-                    eout = new DiscordInteractionResponseBuilder().WithContent($"{Program.cfgjson.Emoji.Error} As a Trial Moderator you cannot perform moderation actions on other staff members or bots.");
-                    eout.IsEphemeral = true;
-                    await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, eout);
+                    webhookOut = new DiscordWebhookBuilder().WithContent($"{Program.cfgjson.Emoji.Error} As a Trial Moderator you cannot perform moderation actions on other staff members or bots.");
+                    await ctx.EditResponseAsync(webhookOut);
                     return;
                 }
             }
@@ -59,105 +63,119 @@ namespace Cliptok.Modules
             var msg = await channel.SendMessageAsync(messageBuild);
             UserWarning warning = await Warnings.GiveWarningAsync(user, ctx.User, reason, Warnings.MessageLink(msg), channel);
 
-            eout = new DiscordInteractionResponseBuilder().WithContent($"{Program.cfgjson.Emoji.Success} User was warned successfully in {channel.Mention}\n[Jump to warning]({Warnings.MessageLink(msg)})");
-            eout.IsEphemeral = true;
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, eout);
+            webhookOut = new DiscordWebhookBuilder().WithContent($"{Program.cfgjson.Emoji.Success} User was warned successfully in {channel.Mention}\n[Jump to warning]({Warnings.MessageLink(msg)})");
+            await ctx.EditResponseAsync(webhookOut);
         }
 
-        //[SlashCommand("ban", "Bans a user from the server, either permanently or temporarily.")]
-        //public async Task BanSlashCommand(InteractionContext ctx,
-        //        [Option("user", "The user to ban")] DiscordUser user,
-        //        [Option("reason", "The reason the user is being banned")] string reason,
-        //        [Option("time", "The length of time the user is banned for, permanent if not specified.")] string time = null,
-        //        [Option("keepMessages", "Whether to keep the users messages when banning")] bool keepMessages = false,
-        //        [Option("appealLink", "Whether to show the user an appeal URL in the ban message.")] bool appealLink = false,
-        //        [Option("silent", "Whether to not send any DM communication about the ban at all")] bool silent = false
-        //)
-        //{
-        //    TimeSpan banDuration = default;
-        //    DiscordInteractionResponseBuilder eout;
+        [SlashCommand("ban", "EXPERIMENTAL: Bans a user from the server, either permanently or temporarily. May contain bugs.")]
+        public async Task BanSlashCommand(InteractionContext ctx,
+                [Option("user", "The user to ban")] DiscordUser user,
+                [Option("reason", "The reason the user is being banned")] string reason,
+                [Option("appeal_link", "Whether to show the user an appeal URL in the DM")] bool appealable = false,
+                [Option("time", "The length of time the user is banned for")] string time = null,
+                [Option("keep_messages", "Whether to keep the users messages when banning")] bool keepMessages = false                
+                //[Option("silent", "Whether to not send any DM communication about the ban at all")] bool silent = false
+        )
+        {
+            // Initial response to avoid the 3 second timeout, will edit later.
+            var eout = new DiscordInteractionResponseBuilder().AsEphemeral(true);
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, eout);
 
-        //    if (Warnings.GetPermLevel(ctx.Member) < ServerPermLevel.TrialMod)
-        //    {
-        //        eout = new DiscordInteractionResponseBuilder().WithContent($"{Program.cfgjson.Emoji.NoPermissions} Invalid permissions to use command **{ctx.CommandName}**!");
-        //        eout.IsEphemeral = true;
-        //        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, eout);
-        //        return;
-        //    }
+            // Edits need a webhook rather than interaction..?
+            DiscordWebhookBuilder webhookOut = new DiscordWebhookBuilder();
 
-        //    DiscordMember targetMember;
+            TimeSpan banDuration = default;
 
-        //    try
-        //    {
-        //        targetMember = await ctx.Guild.GetMemberAsync(user.Id);
-        //        if (Warnings.GetPermLevel(ctx.Member) == ServerPermLevel.TrialMod && (Warnings.GetPermLevel(targetMember) >= ServerPermLevel.TrialMod || targetMember.IsBot))
-        //        {
-        //            eout = new DiscordInteractionResponseBuilder().WithContent($"{Program.cfgjson.Emoji.Error} As a Trial Moderator you cannot perform moderation actions on other staff members or bots.");
-        //            eout.IsEphemeral = true;
-        //            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, eout);
-        //            return;
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        // do nothing :/
-        //    }
+            int messageDeleteDays = 7;
+            if (keepMessages)
+                messageDeleteDays = 0;
 
-        //    if (time == null)
-        //        banDuration = default;
-        //    else
-        //        banDuration = HumanDateParser.HumanDateParser.Parse(time);
+            if (Warnings.GetPermLevel(ctx.Member) < ServerPermLevel.Mod)
+            {
+                webhookOut.Content = $"{Program.cfgjson.Emoji.NoPermissions} Invalid permissions to use command **{ctx.CommandName}**!";
+                await ctx.EditResponseAsync(webhookOut);
+                return;
+            }
 
-        //    if (reason.Length > 6 && reason.Substring(0, 7) == "appeal ")
-        //    {
-        //        appealable = true;
-        //        reason = reason[7..^0];
-        //    }
+            DiscordMember targetMember;
 
-        //    DiscordMember member;
-        //    try
-        //    {
-        //        member = await ctx.Guild.GetMemberAsync(targetMember.Id);
-        //    }
-        //    catch
-        //    {
-        //        member = null;
-        //    }
+            try
+            {
+                targetMember = await ctx.Guild.GetMemberAsync(user.Id);
+                if (Warnings.GetPermLevel(ctx.Member) == ServerPermLevel.TrialMod && (Warnings.GetPermLevel(targetMember) >= ServerPermLevel.TrialMod || targetMember.IsBot))
+                {
+                    webhookOut.Content = $"{Program.cfgjson.Emoji.Error} As a Trial Moderator you cannot perform moderation actions on other staff members or bots.";
+                    await ctx.EditResponseAsync(webhookOut);
+                    return;
+                }
+            }
+            catch
+            {
+                // do nothing :/
+            }
 
-        //    if (member == null)
-        //    {
-        //        await ctx.Message.DeleteAsync();
-        //        await BanFromServerAsync(targetMember.Id, reason, ctx.User.Id, ctx.Guild, 7, ctx.Channel, banDuration, appealable);
-        //    }
-        //    else
-        //    {
-        //        if (AllowedToMod(ctx.Member, member))
-        //        {
-        //            if (AllowedToMod(await ctx.Guild.GetMemberAsync(ctx.Client.CurrentUser.Id), member))
-        //            {
-        //                await ctx.Message.DeleteAsync();
-        //                await BanFromServerAsync(targetMember.Id, reason, ctx.User.Id, ctx.Guild, 7, ctx.Channel, banDuration, appealable);
-        //            }
-        //            else
-        //            {
-        //                await ctx.Channel.SendMessageAsync($"{Program.cfgjson.Emoji.Error} {ctx.User.Mention}, I don't have permission to ban **{targetMember.Username}#{targetMember.Discriminator}**!");
-        //                return;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            await ctx.Channel.SendMessageAsync($"{Program.cfgjson.Emoji.Error} {ctx.User.Mention}, you don't have permission to ban **{targetMember.Username}#{targetMember.Discriminator}**!");
-        //            return;
-        //        }
-        //    }
-        //    reason = reason.Replace("`", "\\`").Replace("*", "\\*");
-        //    if (banDuration == default)
-        //        await ctx.Channel.SendMessageAsync($"{Program.cfgjson.Emoji.Banned} {targetMember.Mention} has been banned: **{reason}**");
-        //    else
-        //        await ctx.Channel.SendMessageAsync($"{Program.cfgjson.Emoji.Banned} {targetMember.Mention} has been banned for **{Warnings.TimeToPrettyFormat(banDuration, false)}**: **{reason}**");
+            if (time == null)
+                banDuration = default;
+            else
+            {
+                try
+                {
+                    banDuration = HumanDateParser.HumanDateParser.Parse(time).Subtract(ctx.Interaction.CreationTimestamp.DateTime);
+                }
+                catch
+                {
+                    webhookOut.Content = $"{Program.cfgjson.Emoji.Error} There was an error parsing your supplied ban length!";
+                    await ctx.EditResponseAsync(webhookOut);
+                    return;
+                }
 
+            }
 
-        //}
+            DiscordMember member;
+            try
+            {
+                member = await ctx.Guild.GetMemberAsync(user.Id);
+            }
+            catch
+            {
+                member = null;
+            }
+
+            if (member == null)
+            {
+                await ModCmds.BanFromServerAsync(user.Id, reason, ctx.User.Id, ctx.Guild, messageDeleteDays, ctx.Channel, banDuration, appealable);
+            }
+            else
+            {
+                if (ModCmds.AllowedToMod(ctx.Member, member))
+                {
+                    if (ModCmds.AllowedToMod(await ctx.Guild.GetMemberAsync(ctx.Client.CurrentUser.Id), member))
+                    {
+                        await ModCmds.BanFromServerAsync(user.Id, reason, ctx.User.Id, ctx.Guild, 7, ctx.Channel, banDuration, appealable);
+                    }
+                    else
+                    {
+                        webhookOut.Content = $"{Program.cfgjson.Emoji.Error} I don't have permission to ban **{user.Username}#{user.Discriminator}**!";
+                        await ctx.EditResponseAsync(webhookOut);
+                        return;
+                    }
+                }
+                else
+                {
+                    webhookOut.Content = $"{Program.cfgjson.Emoji.Error} You don't have permission to ban **{user.Username}#{user.Discriminator}**!";
+                    await ctx.EditResponseAsync(webhookOut);
+                    return;
+                }
+            }
+            reason = reason.Replace("`", "\\`").Replace("*", "\\*");
+            if (banDuration == default)
+                await ctx.Channel.SendMessageAsync($"{Program.cfgjson.Emoji.Banned} {user.Mention} has been banned: **{reason}**");
+            else
+                await ctx.Channel.SendMessageAsync($"{Program.cfgjson.Emoji.Banned} {user.Mention} has been banned for **{Warnings.TimeToPrettyFormat(banDuration, false)}**: **{reason}**");
+
+            webhookOut.Content = $"{Program.cfgjson.Emoji.Success} User was successfully bonked.";
+            await ctx.EditResponseAsync(webhookOut);
+        }
 
     }
 }
