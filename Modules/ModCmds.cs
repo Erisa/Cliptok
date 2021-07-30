92dc9d394bf89789eb153817db6a448da18e50b2
Torch <31840547+torchgm@users.noreply.github.com>
2021-07-30 18:57:21 +0100

    Update !timestamp to support multiple formats (#60)
    * Update !timestamp to support multiple formats
    
    Extends !timestamp to allow the ability to specify the return format of the timestamp as either a full date/time instead of having to check it on other websites, or copy the returned timestamp and then format it yourself as a Discord timestamp, then send that back to the chat again.
    
    I have done this all on my phone whilst i make a sandwich so no idea if it works plz check n test in devtok first thx
    
    * actually use a different class for the group
    
    * fix the sad bit
    
    * Update ModCmds.cs
    
    * Maybe make unix the default return value for !timestamp
    
    * Remove clock from !timestamp default response
    
    * formatting

--- Modules/ModCmds.cs
+++ Modules/ModCmds.cs
@@ -344,15 +344,43 @@ namespace Cliptok.Modules
             await Program.db.ListRightPushAsync("reminders", JsonConvert.SerializeObject(reminderObject));
             await ctx.RespondAsync($"{Program.cfgjson.Emoji.Success} I'll try my best to remind you about that on <t:{ToUnixTimestamp(t)}:f> (<t:{ToUnixTimestamp(t)}:R>)"); // (In roughly **{Warnings.TimeToPrettyFormat(t.Subtract(ctx.Message.Timestamp.DateTime), false)}**)");
         }
-
-        [Command("timestamp")]
+        
+        [Group("timestamp")]
         [Aliases("ts", "time")]
-        [Description("Returns the Unix timestamp of a given Discord ID/snowflake")]
-        public async Task TimestampCmd(CommandContext ctx, [Description("The ID/snowflake to fetch the Unix timestamp for")] ulong snowflake)
+        [Description("Returns various timestamps for a given Discord ID/snowflake")]
+        [HomeServer, RequireHomeserverPerm(ServerPermLevel.Mod)]
+        class TimestampCmds : BaseCommandModule
         {
-            var msSinceEpoch = snowflake >> 22;
-            var msUnix = msSinceEpoch + 1420070400000;
-            await ctx.RespondAsync((msUnix / 1000).ToString());
+            [GroupCommand]
+            [Aliases("u", "unix", "epoch")]
+            [Description("Returns the Unix timestamp of a given Discord ID/snowflake")]
+            public async Task TimestampUnixCmd(CommandContext ctx, [Description("The ID/snowflake to fetch the Unix timestamp for")] ulong snowflake)
+            {
+                var msSinceEpoch = snowflake >> 22;
+                var msUnix = msSinceEpoch + 1420070400000;
+                await ctx.RespondAsync($"{(msUnix / 1000).ToString()}");
+            }
+
+            [Command("relative")]
+            [Aliases("r")]
+            [Description("Returns the amount of time between now and a given Discord ID/snowflake")]
+            public async Task TimestampRelativeCmd(CommandContext ctx, [Description("The ID/snowflake to fetch the relative timestamp for")] ulong snowflake)
+            {
+                var msSinceEpoch = snowflake >> 22;
+                var msUnix = msSinceEpoch + 1420070400000;
+                await ctx.RespondAsync($"{Program.cfgjson.Emoji.ClockTime} <t:{(msUnix / 1000).ToString()}:R>");
+            }
+
+            [Command("fulldate")]
+            [Aliases("f", "datetime")]
+            [Description("Returns the fully-formatted date and time of a given Discord ID/snowflake")]
+            public async Task TimestampFullCmd(CommandContext ctx, [Description("The ID/snowflake to fetch the full timestamp for")] ulong snowflake)
+            {
+                var msSinceEpoch = snowflake >> 22;
+                var msUnix = msSinceEpoch + 1420070400000;
+                await ctx.RespondAsync($"{Program.cfgjson.Emoji.ClockTime} <t:{(msUnix / 1000).ToString()}:F>");
+            }
+
         }
 
         public static long ToUnixTimestamp(DateTime dateTime)
