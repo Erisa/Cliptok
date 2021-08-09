e7a6c3bbdcec95bfd1745fbcf3286f982b711407
Erisa A <seriel@erisa.moe>
2021-08-09 04:20:11 +0100

    Remove unncessary repeated code

--- Program.cs
+++ Program.cs
@@ -216,7 +216,7 @@ namespace Cliptok
                         using var sr = new StreamReader("CommitHash.txt");
                         commitHash = sr.ReadToEnd();
                     }
-                    else if (Environment.GetEnvironmentVariable("RAILWAY_GIT_COMMIT_SHA") != null && Environment.GetEnvironmentVariable("RAILWAY_GIT_COMMIT_SHA").All(char.IsWhiteSpace))
+                    else if (Environment.GetEnvironmentVariable("RAILWAY_GIT_COMMIT_SHA") != null)
                     {
                         commitHash = Environment.GetEnvironmentVariable("RAILWAY_GIT_COMMIT_SHA");
                         commitHash = commitHash.Substring(0, Math.Min(commitHash.Length, 7));
@@ -232,7 +232,7 @@ namespace Cliptok
                         using var sr = new StreamReader("CommitMessage.txt");
                         commitMessage = sr.ReadToEnd();
                     }
-                    else if (Environment.GetEnvironmentVariable("RAILWAY_GIT_COMMIT_MESSAGE") != null && Environment.GetEnvironmentVariable("RAILWAY_GIT_COMMIT_MESSAGE").All(char.IsWhiteSpace))
+                    else if (Environment.GetEnvironmentVariable("RAILWAY_GIT_COMMIT_MESSAGE") != null)
                     {
                         commitMessage = Environment.GetEnvironmentVariable("RAILWAY_GIT_COMMIT_MESSAGE");
                     }
