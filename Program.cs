682c2f7772f209f6205f2e4fbaf0043a2d572a60
Erisa A <erisa@erisa.uk>
2021-09-13 23:03:58 +0100

    Delay anti-heartosoft by 1s to avoid starboard race conditions

--- Program.cs
+++ Program.cs
@@ -182,6 +182,8 @@ namespace Cliptok
 
                     DiscordEmoji noHeartosoft = await e.Guild.GetEmojiAsync(cfgjson.NoHeartosoftId);
 
+                    await Task.Delay(1000);
+
                     if (targetMessage.Author.Id == e.User.Id)
                     {
                         await targetMessage.DeleteReactionAsync(e.Emoji, e.User);
