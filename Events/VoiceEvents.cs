5f6676be16db23913cb4dce8cfc1234a165ce0c7
Erisa A <erisa@erisa.uk>
2022-07-29 06:15:08 +0100

    Implement voice channel purging

--- Events/VoiceEvents.cs
+++ Events/VoiceEvents.cs
@@ -2,37 +2,83 @@
 {
     public class VoiceEvents
     {
+
         private static List<ulong> PendingOverWrites = new();
+        private static List<ulong> PendingPurge = new();
 
         public static async Task VoiceStateUpdate(DiscordClient client, VoiceStateUpdateEventArgs e)
         {
-            if (e.After.Channel is null)
+            Task.Run(async () =>
             {
-                client.Logger.LogDebug($"{e.User.Username} left {e.Before.Channel.Name}");
+                if (e.After.Channel is null)
+                {
+                    client.Logger.LogDebug($"{e.User.Username} left {e.Before.Channel.Name}");
 
-                UserLeft(client, e);
-            }
-            else if (e.Before is null)
-            {
-                client.Logger.LogDebug($"{e.User.Username} joined {e.After.Channel.Name}");
+                    UserLeft(client, e);
+                }
+                else if (e.Before is null)
+                {
+                    client.Logger.LogDebug($"{e.User.Username} joined {e.After.Channel.Name}");
 
-                UserJoined(client, e);
-            }
-            else if (e.Before.Channel.Id != e.After.Channel.Id)
-            {
-                client.Logger.LogDebug($"{e.User.Username} moved from {e.Before.Channel.Name} to {e.After.Channel.Name}");
+                    UserJoined(client, e);
+                }
+                else if (e.Before.Channel.Id != e.After.Channel.Id)
+                {
+                    client.Logger.LogDebug($"{e.User.Username} moved from {e.Before.Channel.Name} to {e.After.Channel.Name}");
 
-                UserLeft(client, e);
-                UserJoined(client, e);
-            }
+                    UserLeft(client, e);
+                    UserJoined(client, e);
+                }
 
-            if (e.Before is not null && e.Before.Channel.Users.Count == 0)
-            {
-                client.Logger.LogDebug($"{e.Before.Channel.Name} is now empty!");
+                if (e.Before is not null && e.Before.Channel.Users.Count == 0)
+                {
+                    client.Logger.LogDebug($"{e.Before.Channel.Name} is now empty!");
 
-                // todo: purge message history, on delay
-            }
+                    Task.Run(async () =>
+                    {
 
+                        if (PendingPurge.Contains(e.Before.Channel.Id))
+                            return;
+
+                        PendingPurge.Add(e.Before.Channel.Id);
+
+                        for (int i = 0; i <= 12; i++)
+                        {
+                            if (e.Guild.Channels[e.Before.Channel.Id].Users.Count != 0)
+                            {
+                                PendingPurge.Remove(e.Before.Channel.Id);
+                                return;
+                            }
+                            else
+                            {
+                                await Task.Delay(10000);
+                            }
+                        }
+
+                        if (e.Guild.Channels[e.Before.Channel.Id].Users.Count == 0)
+                        {
+                            List<DiscordMessage> messages = new();
+                            var firstMsg = (await e.Before.Channel.GetMessagesAsync(1)).First();
+                            messages.Add(firstMsg);
+                            var lastMsgId = firstMsg.Id;
+                            // delete all the messages from the channel
+                            while (true)
+                            {
+                                var newmsgs = (await e.Before.Channel.GetMessagesBeforeAsync(lastMsgId, 100)).ToList();
+                                messages.AddRange(newmsgs);
+                                lastMsgId = newmsgs.Last().Id;
+                                if (newmsgs.Count < 100)
+                                {
+                                    break;
+                                }
+                            }
+                            messages.RemoveAll(message => message.CreationTimestamp.ToUniversalTime() < DateTime.UtcNow.AddDays(-14));
+                            PendingPurge.Remove(e.Before.Channel.Id);
+                            await e.Before.Channel.DeleteMessagesAsync(messages);
+                        }
+                    });
+                }
+            });
         }
 
         public static async Task UserJoined(DiscordClient _, VoiceStateUpdateEventArgs e)
@@ -81,61 +127,54 @@
 
         public static async Task UserLeft(DiscordClient _, VoiceStateUpdateEventArgs e)
         {
-            Task.Run(async () =>
+            DiscordMember member = e.After.Member;
+
+            while (PendingOverWrites.Contains(e.User.Id)) ;
             {
-                DiscordMember member;
-                if (e.After.Channel is null)
-                    member = e.Before.Member;
-                else
-                    member = e.After.Member;
+                Console.WriteLine("spinning");
+                await Task.Delay(5);
+            }
 
-                while (PendingOverWrites.Contains(e.User.Id));
+            PendingOverWrites.Add(e.User.Id);
+
+            DiscordOverwrite[] existingOverwrites = e.Before.Channel.PermissionOverwrites.ToArray();
+
+            foreach (DiscordOverwrite overwrite in existingOverwrites)
+            {
+                if (overwrite.Type == OverwriteType.Member && overwrite.Id == member.Id)
                 {
-                    Console.WriteLine("spinning");
-                    await Task.Delay(5);
-                }
-
-                PendingOverWrites.Add(e.User.Id);
-
-                DiscordOverwrite[] existingOverwrites = e.Before.Channel.PermissionOverwrites.ToArray();
-
-                foreach (DiscordOverwrite overwrite in existingOverwrites)
-                {
-                    if (overwrite.Type == OverwriteType.Member && overwrite.Id == member.Id)
+                    if (overwrite.Allowed == Permissions.SendMessages && overwrite.Denied == Permissions.None)
                     {
-                        if (overwrite.Allowed == Permissions.SendMessages && overwrite.Denied == Permissions.None)
+                        // User only has allow for Send Messages, so we can delete the entire override
+                        await overwrite.DeleteAsync("User left voice channel.");
+                    }
+                    else
+                    {
+                        // User has other overrides set, so we should only remove the Send Messages override
+                        if (overwrite.Allowed.HasPermission(Permissions.SendMessages))
                         {
-                            // User only has allow for Send Messages, so we can delete the entire override
-                            await overwrite.DeleteAsync("User left voice channel.");
+                            await e.Before.Channel.AddOverwriteAsync(member, (Permissions)(overwrite.Allowed - Permissions.SendMessages), overwrite.Denied, "User left voice channel.");
                         }
                         else
                         {
-                            // User has other overrides set, so we should only remove the Send Messages override
-                            if (overwrite.Allowed.HasPermission(Permissions.SendMessages))
+                            // Check if the overwrite has no permissions set - if so, delete it to keep the list clean.
+                            if (overwrite.Allowed == Permissions.None && overwrite.Denied == Permissions.None)
                             {
-                                await e.Before.Channel.AddOverwriteAsync(member, (Permissions)(overwrite.Allowed - Permissions.SendMessages), overwrite.Denied, "User left voice channel.");
-                            }
-                            else
-                            {
-                                // Check if the overwrite has no permissions set - if so, delete it to keep the list clean.
-                                if (overwrite.Allowed == Permissions.None && overwrite.Denied == Permissions.None)
-                                {
-                                    await overwrite.DeleteAsync("User left voice channel.");
-                                }
+                                await overwrite.DeleteAsync("User left voice channel.");
                             }
                         }
-                        break;
                     }
+                    break;
                 }
+            }
 
-                PendingOverWrites.Remove(e.User.Id);
+            PendingOverWrites.Remove(e.User.Id);
 
-                DiscordMessageBuilder message = new()
-                {
-                    Content = $"{member.Mention} has left."
-                };
-                await e.Before.Channel.SendMessageAsync(message.WithAllowedMentions(Mentions.None));
-            });
+            DiscordMessageBuilder message = new()
+            {
+                Content = $"{member.Mention} has left."
+            };
+            await e.Before.Channel.SendMessageAsync(message.WithAllowedMentions(Mentions.None));
         }
     }
 }
