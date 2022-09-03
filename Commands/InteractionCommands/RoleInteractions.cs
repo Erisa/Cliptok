b5fbf21e2e5f3bf81d6bc0becf97ec06e2cc1341
Erisa A <erisa@erisa.uk>
2022-09-03 12:39:22 +0100

    Correct the permission requirement for /grant

--- Commands/InteractionCommands/RoleInteractions.cs
+++ Commands/InteractionCommands/RoleInteractions.cs
@@ -3,7 +3,7 @@
     internal class RoleInteractions : ApplicationCommandModule
     {
         [SlashCommand("grant", "Grant a user Tier 1, bypassing any verification requirements.")]
-        [SlashRequireHomeserverPerm(ServerPermLevel.Moderator), SlashCommandPermissions(Permissions.ModerateMembers)]
+        [SlashRequireHomeserverPerm(ServerPermLevel.TrialModerator), SlashCommandPermissions(Permissions.ModerateMembers)]
         public async Task SlashGrant(InteractionContext ctx, [Option("user", "THe user to grant Tier 1 to.")] DiscordUser user)
         {
             DiscordMember member = default;
@@ -19,7 +19,7 @@
 
             var tierOne = ctx.Guild.GetRole(Program.cfgjson.TierRoles[0]);
 
-            await member.GrantRoleAsync(tierOne, $"!grant used by {ctx.User.Username}#{ctx.User.Discriminator}");
+            await member.GrantRoleAsync(tierOne, $"/grant used by {ctx.User.Username}#{ctx.User.Discriminator}");
             await ctx.RespondAsync($"{Program.cfgjson.Emoji.Success} {member.Mention} can now access the server!");
         }
 
