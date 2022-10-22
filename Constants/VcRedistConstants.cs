b823e8872023da34532539519bd8d40b48406f8a
Nikhil B <59918974+heftymouse@users.noreply.github.com>
2022-10-22 12:30:06 +0530

    Remove redundant stuff

--- Constants/VcRedistConstants.cs
+++ Constants/VcRedistConstants.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DescriptionAttribute = System.ComponentModel.DescriptionAttribute;
-
-namespace Cliptok.Constants
+﻿namespace Cliptok.Constants
 {
     internal class VcRedistConstants
     {
