0d7b88005fc424592c563905bdcdeef8f0afbbf0
Nikhil B <59918974+heftymouse@users.noreply.github.com>
2022-10-22 10:15:22 +0530

    Added /vcredist for tech support

--- Commands/Dehoist.cs
+++ Commands/Dehoist.cs
@@ -97,9 +97,9 @@
             else
             {
                 string strList;
-                using (WebClient client = new())
+                using(HttpClient client = new())
                 {
-                    strList = client.DownloadString(ctx.Message.Attachments[0].Url);
+                    strList = await client.GetStringAsync(ctx.Message.Attachments[0].Url);
                 }
 
                 var list = strList.Split(' ');
---
+++ Commands/InteractionCommands/TechSupportInteractions.cs
@@ -0,0 +1,53 @@
+﻿using Cliptok.Constants;
+using Emzi0767.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliptok.Commands.InteractionCommands
+{
+    internal class TechSupportInteractions : ApplicationCommandModule
+    {
+        [SlashCommand("vcredist", "Outputs download URLs for the specified Visual C++ Redistributables version")]
+        public async Task RedistsCommand(
+            InteractionContext ctx,
+            [Option("version", "Visual Studio version number or year")] long version
+            )
+        {
+            VcRedist redist = VcRedistConstants.VcRedists
+                .Where((e) =>
+                {
+                    if (version is (>= 2015 or 140))
+                    {
+                        return e.Year == 2015;
+                    }
+                    else
+                    {
+                        return version == e.Year || version == e.Version;
+                    }
+                })
+                .FirstOrDefault();
+
+            DiscordEmbedBuilder embed = new DiscordEmbedBuilder()
+                .WithColor(new("7160e8"));
+
+            if (redist.Year != 0)
+            {
+                foreach (var url in redist.DownloadUrls)
+                {
+                    embed.AddField($"{url.Key.ToString("G")}", $"{url.Value}");
+                }
+                embed.WithTitle($"Visual C++ {redist.Year}{(redist.Year == 2015 ? "+" : "")} Redistributables (version {redist.Version})")
+                    .WithFooter("The above links are official and safe to download.");
+            }
+            else
+            {
+                embed.WithDescription("No results were found for the specified version or year.");
+            }
+
+            await ctx.RespondAsync(null, embed.Build(), false);
+        }
+    }
+}
