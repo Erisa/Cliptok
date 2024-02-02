57931abdc0015bc4639db7831d72b8bf4f848a9c
Erisa A <erisa@erisa.uk>
2024-02-02 05:22:35 +0000

    Double check that WS is connected before heartbeating

--- Events/HeartbeatEvent.cs
+++ Events/HeartbeatEvent.cs
@@ -4,7 +4,7 @@
     {
         public static async Task OnHeartbeat(DiscordClient client, HeartbeatEventArgs e)
         {
-            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("UPTIME_KUMA_PUSH_URL")))
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("UPTIME_KUMA_PUSH_URL")) && client.IsConnected)
             {
                 var response = await Program.httpClient.GetAsync(Environment.GetEnvironmentVariable("UPTIME_KUMA_PUSH_URL") + client.Ping);
                 client.Logger.LogDebug("Heartbeat ping sent: {status} {content}", (int)response.StatusCode, await response.Content.ReadAsStringAsync());
