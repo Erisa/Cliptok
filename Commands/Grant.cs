4ed4d92e771ab8f4d85a403515e6f0d984e5a9e7
Erisa A <erisa@erisa.uk>
2024-05-20 19:37:24 +0100

    Deprecate /grant and !grant command

--- Commands/Grant.cs
+++ Commands/Grant.cs
@@ -6,22 +6,9 @@
         [Description("Grant a user access to the server, by giving them the Tier 1 role.")]
         [Aliases("clipgrant", "verify")]
         [HomeServer, RequireHomeserverPerm(ServerPermLevel.TrialModerator)]
-        public async Task GrantCommand(CommandContext ctx, [Description("The member to grant Tier 1 role to.")] DiscordUser user)
+        public async Task GrantCommand(CommandContext ctx, [Description("The member to grant Tier 1 role to.")] DiscordUser _)
         {
-            DiscordMember member = default;
-            try
-            {
-                member = await ctx.Guild.GetMemberAsync(user.Id);
-            }
-            catch (Exception)
-            {
-                await ctx.RespondAsync($"{Program.cfgjson.Emoji.Error} That user does not appear to be in the server!");
-                return;
-            }
-
-            var tierOne = ctx.Guild.GetRole(Program.cfgjson.TierRoles[0]);
-            await member.GrantRoleAsync(tierOne, $"!grant used by {DiscordHelpers.UniqueUsername(ctx.User)}");
-            await ctx.RespondAsync($"{Program.cfgjson.Emoji.Success} {member.Mention} can now access the server(?)\n**WARNING**: This probably did not work due to recent Discord changes. Please use a desktop or web Discord client, right click the user and click \"Verify Member\" if available.");
+            await ctx.RespondAsync($"{Program.cfgjson.Emoji.Error} This command is deprecated and no longer works. Please right click (or tap and hold on mobile) the user and click \"Verify Member\" if available.");
         }
     }
 }
