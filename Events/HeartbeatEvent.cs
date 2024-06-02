308a800b5a44640ecd431eb6c68b1a442e60878f
Erisa A <erisa@erisa.uk>
2024-06-03 00:38:08 +0100

    Log heartbeat errors

--- Events/HeartbeatEvent.cs
+++ Events/HeartbeatEvent.cs
@@ -7,7 +7,12 @@
             if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("UPTIME_KUMA_PUSH_URL")) && client.IsConnected)
             {
                 var response = await Program.httpClient.GetAsync(Environment.GetEnvironmentVariable("UPTIME_KUMA_PUSH_URL") + client.Ping);
-                client.Logger.LogDebug("Heartbeat ping sent: {status} {content}", (int)response.StatusCode, await response.Content.ReadAsStringAsync());
+                if (response.StatusCode == HttpStatusCode.OK) {
+                    client.Logger.LogDebug("Heartbeat ping succeeded.");
+                } else
+                {
+                    client.Logger.LogError("Heartbeat ping sent: {status} {content}", (int)response.StatusCode, await response.Content.ReadAsStringAsync());
+                }
                 return;
             }
         }
