d4a11cde11ee47de0ef9dd2ab1e32b62973a0d52
FloatingMilkshake <me@floatingmilkshake.com>
2024-08-09 22:00:58 -0400

    Add null checks to Message Create and Message Update debug logs

--- Events/MessageEvent.cs
+++ Events/MessageEvent.cs
@@ -16,14 +16,34 @@ namespace Cliptok.Events
 
         public static async Task MessageCreated(DiscordClient client, MessageCreatedEventArgs e)
         {
-            client.Logger.LogDebug("Got a message create event for {message} by {user}", DiscordHelpers.MessageLink(e.Message), e.Message.Author.Id);
+            if (e.Message is null)
+            {
+                client.Logger.LogError("Got a message create event but the message was null!");
+                return;
+            } else if (e.Message.Author is null)
+            {
+                client.Logger.LogDebug("Got a message create event for a message with no author: {message}", DiscordHelpers.MessageLink(e.Message));
+            } else
+            {
+                client.Logger.LogDebug("Got a message create event for {message} by {user}", DiscordHelpers.MessageLink(e.Message), e.Message.Author.Id);
+            }
 
             await MessageHandlerAsync(client, e.Message, e.Channel);
         }
 
         public static async Task MessageUpdated(DiscordClient client, MessageUpdatedEventArgs e)
         {
-            client.Logger.LogDebug("Got a message update event for {message} by {user}", DiscordHelpers.MessageLink(e.Message), e.Message.Author.Id);
+            if (e.Message is null)
+            {
+                client.Logger.LogError("Got a message update event but the message was null!");
+                return;
+            } else if (e.Message.Author is null)
+            {
+                client.Logger.LogDebug("Got a message update event for a message with no author: {message}", DiscordHelpers.MessageLink(e.Message));
+            } else
+            {
+                client.Logger.LogDebug("Got a message update event for {message} by {user}", DiscordHelpers.MessageLink(e.Message), e.Message.Author.Id);
+            }
 
             await MessageHandlerAsync(client, e.Message, e.Channel, true);
         }
