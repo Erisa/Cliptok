fba606ca55a323c5968b55cbf92e1c143f26fd18
Erisa A <erisa@erisa.uk>
2024-10-10 22:03:23 +0100

    Apparently the on-call footers were not actually footers

--- Commands/TechSupportCommands.cs
+++ Commands/TechSupportCommands.cs
@@ -11,8 +11,7 @@
             await ctx.Member.GrantRoleAsync(ctsRole, "Used !on-call");
             await ctx.RespondAsync(new DiscordMessageBuilder().AddEmbed(new DiscordEmbedBuilder()
                 .WithTitle($"{Program.cfgjson.Emoji.On} Received Community Tech Support Role")
-                .WithDescription($"{ctx.User.Mention} is available to help out in **#tech-support**.")
-                .WithFooter("(Use `!off-call` when you're no longer available)")
+                .WithDescription($"{ctx.User.Mention} is available to help out in **#tech-support**.\n(Use `!off-call` when you're no longer available)")
                 .WithColor(DiscordColor.Green)
             ));
         }
@@ -26,8 +25,7 @@
             await ctx.Member.RevokeRoleAsync(ctsRole, "Used !off-call");
             await ctx.RespondAsync(new DiscordMessageBuilder().AddEmbed(new DiscordEmbedBuilder()
                 .WithTitle($"{Program.cfgjson.Emoji.Off} Removed Community Tech Support Role")
-                .WithDescription($"{ctx.User.Mention} is no longer available to help out in **#tech-support**.")
-                .WithFooter("(Use `!on-call` again when you're available)")
+                .WithDescription($"{ctx.User.Mention} is no longer available to help out in **#tech-support**.\n(Use `!on-call` again when you're available)")
                 .WithColor(DiscordColor.Red)
             ));
         }
