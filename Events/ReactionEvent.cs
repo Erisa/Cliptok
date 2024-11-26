9345ebb5d817473ea0e229e07ae2b863e5223a15
FloatingMilkshake <me@floatingmilkshake.com>
2024-11-26 15:36:08 -0500

    Automatically remove reactions to warning/mute/ban messages
    Closes #244

--- Events/ReactionEvent.cs
+++ Events/ReactionEvent.cs
@@ -6,11 +6,29 @@ namespace Cliptok.Events
     {
         public static async Task OnReaction(DiscordClient _, MessageReactionAddedEventArgs e)
         {
-            if (e.Emoji.Id != cfgjson.HeartosoftId || e.Channel.IsPrivate || e.Guild.Id != cfgjson.ServerID)
+            // Ignore DMs and other servers
+            if (e.Channel.IsPrivate || e.Guild.Id != cfgjson.ServerID)
                 return;
 
             DiscordMessage targetMessage = await e.Channel.GetMessageAsync(e.Message.Id);
 
+            // Remove reactions from warning/mute/ban messages
+
+            if (targetMessage.Author.Id == discord.CurrentUser.Id &&
+                targetMessage.Content.Contains("was warned") ||
+                targetMessage.Content.Contains("has been muted") ||
+                targetMessage.Content.Contains("has been banned"))
+            {
+                await targetMessage.DeleteReactionAsync(e.Emoji, e.User);
+                return;
+            }
+
+            // Remove self-heartosofts
+
+            if (e.Emoji.Id != cfgjson.HeartosoftId)
+                return;
+
+            // Avoid starboard race conditions
             await Task.Delay(1000);
 
             if (targetMessage.Author.Id == e.User.Id)
