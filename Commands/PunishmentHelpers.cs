62d54866ca5a00a16e6ac2e250976cc8e624cb99
Milkshake <me@floatingmilkshake.com>
2025-04-05 19:33:23 -0400

    Add !editban and !editmute (#291)

---
+++ Commands/PunishmentHelpers.cs
@@ -0,0 +1,42 @@
+namespace Cliptok.Commands
+{
+    public class PunishmentHelpers
+    {
+        public static (TimeSpan duration, string reason, bool appealable) UnpackTimeAndReason(string timeAndReason, DateTime anchorTime)
+        {
+            TimeSpan duration = default;
+            bool appealable = false;
+            bool timeParsed = false;
+
+            string possibleTime = timeAndReason.Split(' ').First();
+            try
+            {
+                duration = HumanDateParser.HumanDateParser.Parse(possibleTime).Subtract(anchorTime);
+                timeParsed = true;
+            }
+            catch
+            {
+                // keep default
+            }
+
+            string reason = timeAndReason;
+
+            if (timeParsed)
+            {
+                int i = reason.IndexOf(" ") + 1;
+                reason = reason[i..];
+            }
+
+            if (timeParsed && possibleTime == reason)
+                reason = "No reason specified.";
+
+            if (reason.Length > 6 && reason[..7].ToLower() == "appeal ")
+            {
+                appealable = true;
+                reason = reason[7..^0];
+            }
+
+            return (duration, reason, appealable);
+        }
+    }
+}
