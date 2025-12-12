590e6c0975125ce82cf1066b53847b94977ebc38
FloatingMilkshake <me@floatingmilkshake.com>
2025-12-12 14:12:33 -0500

    Use inline code formatting for Match field

--- Events/MessageEvent.cs
+++ Events/MessageEvent.cs
@@ -715,7 +715,7 @@ namespace Cliptok.Events
                                 DiscordMessage msg = await WarningHelpers.SendPublicWarningMessageAndDeleteInfringingMessageAsync(message, $"{Program.cfgjson.Emoji.Denied} {message.Author.Mention} was automatically warned: **{reason.Replace("`", "\\`").Replace("*", "\\*")}**", wasAutoModBlock, 1);
                                 var warning = await WarningHelpers.GiveWarningAsync(message.Author, client.CurrentUser, reason, contextMessage: msg, channel, " automatically ");
                                 if (listItem.Name == "exploits.txt")
-                                    await InvestigationsHelpers.SendInfringingMessaageAsync("investigations", message, reason, warning.ContextLink, extraField: ("Match", $"```\n{flaggedWord}\n```", true), messageContentOverride: msgContentWithEmbedData, wasAutoModBlock: wasAutoModBlock, useCodeBlock: true);
+                                    await InvestigationsHelpers.SendInfringingMessaageAsync("investigations", message, reason, warning.ContextLink, extraField: ("Match", $"`{flaggedWord}`", true), messageContentOverride: msgContentWithEmbedData, wasAutoModBlock: wasAutoModBlock, useCodeBlock: true);
                                 else
                                     await InvestigationsHelpers.SendInfringingMessaageAsync("investigations", message, reason, warning.ContextLink, extraField: ("Match", flaggedWord, true), messageContentOverride: msgContentWithEmbedData, wasAutoModBlock: wasAutoModBlock);
                                 return;
