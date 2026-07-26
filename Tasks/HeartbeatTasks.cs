64f84d672ac46c82b8f9c03ced71661aa4097362
Milkshake <me@floatingmilkshake.com>
2026-07-25 22:16:51 -0400

    Upgrade DSharpPlus to 5.0.0-nightly-02594 (#364)
    * Upgrade DSharpPlus to 5.0.0-nightly-02594
    
    * Heartbeat every ~50 seconds, instead of every 10 seconds
    
    * Reduce duplicate code

--- Tasks/HeartbeatTasks.cs
+++ Tasks/HeartbeatTasks.cs
@@ -1,34 +1,35 @@
-﻿using DSharpPlus.Net.Gateway;
-
-namespace Cliptok.Events
+namespace Cliptok.Tasks
 {
-    public class HeartbeatEvent
+    public class HeartbeatTasks
     {
-        public static async Task OnHeartbeat(IGatewayClient client)
+        public static async Task<bool> HeartbeatAsync()
         {
-            Program.discord.Logger.LogDebug("Heartbeat ping: {ping}", client.Ping.Milliseconds);
-            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("UPTIME_KUMA_PUSH_URL")) && client.IsConnected)
+            var ping = Program.discord.GetConnectionLatency(Program.homeGuild.Id).Milliseconds;
+            Program.discord.Logger.LogDebug("Heartbeat ping: {ping}", ping);
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("UPTIME_KUMA_PUSH_URL")) && Program.discord.AllShardsConnected)
             {
                 HttpResponseMessage response;
                 try
                 {
-                    response = await Program.httpClient.GetAsync(Environment.GetEnvironmentVariable("UPTIME_KUMA_PUSH_URL") + client.Ping.Milliseconds);
+                    response = await Program.httpClient.GetAsync(Environment.GetEnvironmentVariable("UPTIME_KUMA_PUSH_URL") + ping);
                 }
                 catch (Exception ex)
                 {
                     Program.discord.Logger.LogError(ex, "Uptime Kuma push failed during heartbeat event!");
-                    return;
+                    return false;
                 }
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
                     Program.discord.Logger.LogDebug("Heartbeat ping succeeded.");
+                    return true;
                 }
                 else
                 {
                     Program.discord.Logger.LogError("Heartbeat ping sent: {status} {content}", (int)response.StatusCode, await response.Content.ReadAsStringAsync());
+                    return false;
                 }
-                return;
             }
+            return false;
         }
     }
 }
